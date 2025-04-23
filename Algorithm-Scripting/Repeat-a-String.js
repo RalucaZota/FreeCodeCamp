3f9d4a966f2664a867bbc1601fe9fe6f76ee0189
RalucaZota <zotaraluca649@gmail.com>
2025-04-23 21:18:30 +0300

    Repeat-a-String

--- Algorithm-Scripting/Repeat-a-String.js
+++ Algorithm-Scripting/Repeat-a-String.js
@@ -3,16 +3,10 @@ Repeat a given string (first argument) num times (second argument).
 Return an empty string if num is not a positive number.
 */
 
-function repeatStringNumTimes(str, num) {
-  var newString = "";
-  if (num > 0) {
-    for (i=1; i<=num; i++) {
-      newString = newString + str;
-    }
-    return newString;
-  } else {
-    return "";
-  }
+function repeatStringNumTimes(str, num){
+  if(num < 0) return ""
+  const repeteadString = str.repeat(num)
+  return repeteadString
 }
 
 repeatStringNumTimes("abc", 3);   // "abcabcabc"
