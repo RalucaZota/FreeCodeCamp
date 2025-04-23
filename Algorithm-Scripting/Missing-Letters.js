6467f55759b006f25d7eaedf4114898d19a81e42
RalucaZota <zotaraluca649@gmail.com>
2025-04-23 19:58:57 +0300

    Missing-Letters.js

--- Algorithm-Scripting/Missing-Letters.js
+++ Algorithm-Scripting/Missing-Letters.js
@@ -4,18 +4,22 @@ Find the missing letter in the passed letter range and return it.
 If all letters are present in the range, return undefined.
 */
 
-function fearNotLetter(str) {
-  var strArr = str.split('');
-  for (i=0; i<strArr.length-1; i++){
-    var curLetter = strArr[i].charCodeAt();
-    var nextLetter = strArr[i+1].charCodeAt();
-    if (nextLetter - curLetter > 1) {
-      return String.fromCharCode(curLetter+1);
-    }
+function findMissingLetter(str) {
+  const lettersArr = [...str];
+  const firstCode = lettersArr[0].charCodeAt(0);
+  const letterCode = lettersArr.find((letter, index) => {
+    const expectedCode = firstCode + index;
+    return letter.charCodeAt(0) !== expectedCode;
+  });
+  if (letterCode) {
+    const index = lettersArr.indexOf(letterCode);
+    const missingLetter = String.fromCharCode(firstCode + index);
+    return missingLetter;
   }
+  return undefined;
 }
 
-fearNotLetter("abce");   // "d"
-fearNotLetter("abcdefghjklmno");   // "i"
-fearNotLetter("bcd");   // undefined
-fearNotLetter("yz");   // undefined
+findMissingLetter('abce'); // "d"
+findMissingLetter('abcdefghjklmno'); // "i"
+findMissingLetter('bcd'); // undefined
+findMissingLetter('yz'); // undefined
