5271af8a289936d86d9d5ef672e38a225baf0084
RalucaZota <zotaraluca649@gmail.com>
2025-04-25 12:28:08 +0300

    Chunky-Monkey.js

--- Algorithm-Scripting/Chunky-Monkey.js
+++ Algorithm-Scripting/Chunky-Monkey.js
@@ -2,26 +2,26 @@
 Write a function that splits an array (first argument) into groups the length of size (second argument)
 and returns them as a two-dimensional array.
 */
-
-function chunkArrayInGroups(arr, size) {
-  var lastPos = 0;
-  var currPos = size;
-  var subArray = [];
-  var newArray = [];
-  while (lastPos < arr.length) {
-    subArray = arr.slice(lastPos, currPos);
-    lastPos += size;
-    currPos += size;
-    newArray.push(subArray);
-    subArray = [];
+function chunkArrayInGroups(arr, size){
+  const newArr  = [];
+  let newSubArr = []
+  for(let i = 0; i < arr.length; i++){
+    newSubArr.push(arr[i]);   
+    if( newSubArr.length === size) {
+      newArr.push(newSubArr)
+      newSubArr = [];
+    }
   }
-  return newArray;
+  if (newSubArr.length > 0) {
+    newArr.push(newSubArr);
+  }
+  return newArr;
 }
-
-chunkArrayInGroups(["a", "b", "c", "d"], 2);   // [["a", "b"], ["c", "d"]]
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);   // [[0, 1, 2], [3, 4, 5]]
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);   // [[0, 1], [2, 3], [4, 5]]
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);   // [[0, 1, 2, 3], [4, 5]]
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);   // [[0, 1, 2], [3, 4, 5], [6]]
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);   // [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);   // [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
+  
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4); // [["a", "b"], ["c", "d"]]
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);   // [[0, 1, 2], [3, 4, 5]]
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);   // [[0, 1], [2, 3], [4, 5]]
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);   // [[0, 1, 2, 3], [4, 5]]
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);   // [[0, 1, 2], [3, 4, 5], [6]]
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);   // [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);   // [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
