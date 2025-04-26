a35e933d199c0796777efbe27278b31eb34ac98e
RalucaZota <zotaraluca649@gmail.com>
2025-04-26 12:56:29 +0300

    Confirm-the-Ending.js

--- Algorithm-Scripting/Confirm-the-Ending.js
+++ Algorithm-Scripting/Confirm-the-Ending.js
@@ -6,13 +6,15 @@ This challenge can be solved with the .endsWith() method, which was introduced i
 */
 
 function confirmEnding(str, target) {
-  return target === (str.substr(-target.length));
+  const targetLength = target.length;
+  const strEnding = str.slice(-targetLength);
+  return strEnding === target
 }
 
-confirmEnding("Bastian", "n");   // true
-confirmEnding("Connor", "n");   // false
-confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification");   // false
-confirmEnding("He has to give me a new name", "name");   // true
-confirmEnding("Open sesame", "same");   // true
-confirmEnding("Open sesame", "pen");   // false
-confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain");   // false
+// confirmEnding("Bastian", "n");   // true
+// confirmEnding("Connor", "n");   // false
+// confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification");   // false
+// confirmEnding("He has to give me a new name", "name");   // true
+// confirmEnding("Open sesame", "same");   // true
+// confirmEnding("Open sesame", "pen");   // false
+// confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain");   // false
