35a9be5ad875c511dbe39321ea34072d1245d82e
RalucaZota <zotaraluca649@gmail.com>
2025-04-27 18:57:53 +0300

    Caesars-Cipher.js

--- Algorithm-Scripting/Caesars-Cipher.js
+++ Algorithm-Scripting/Caesars-Cipher.js
@@ -12,23 +12,24 @@ but do pass them on.
 */
 
 function rot13(str) {
-  var myArray = str.split('');
-  var newArray = [];
-  for (i=0; i<myArray.length; i++) {
-    var myLetter = myArray[i].charCodeAt();
-    var newLetter = myLetter + 13;
-    if (myLetter < 65) {
-      newLetter = myLetter;
+  const newArr = [...str];
+  const codesArr = newArr.map((letter) => {
+    if (letter !== ' ' && !letter.match(/[.,!?;:]/)) {
+      const letterCode = letter.charCodeAt(0);
+      if (letterCode >= 65 && letterCode < 90) {
+        let newLetterCode = letterCode + 13;
+        if (newLetterCode > 90) {
+          newLetterCode -= 26;
+        }
+        return String.fromCharCode(newLetterCode);
+      }
     }
-    if (newLetter > 90) {
-      newLetter = 65 + 13 - 91 + myArray[i].charCodeAt();
-    }
-    newArray.push(String.fromCharCode(newLetter));
-  }
-  return newArray.join('');
+    return letter;
+  });
+  return codesArr.join('');
 }
 
-rot13("SERR CVMMN!");   // "FREE PIZZA!"
-rot13("SERR PBQR PNZC");   // "FREE CODE CAMP"
-rot13("SERR YBIR?");   // "FREE LOVE?"
-rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.");   // "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
+// rot13("SERR CVMMN!");   // "FREE PIZZA!"
+// rot13("SERR PBQR PNZC");   // "FREE CODE CAMP"
+// rot13("SERR YBIR?");   // "FREE LOVE?"
+// rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.");   // "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
