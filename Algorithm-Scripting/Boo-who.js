118ac2758220c5e869dff7d70365d0c779fe3954
RalucaZota <zotaraluca649@gmail.com>
2025-04-27 12:35:50 +0300

    Boo-who.js

--- Algorithm-Scripting/Boo-who.js
+++ Algorithm-Scripting/Boo-who.js
@@ -5,20 +5,17 @@ Boolean primitives are true and false.
 */
 
 function booWho(bool) {
-  if (bool === true || bool === false){
-    return true;
-  }
-  return false;
+ return typeof(bool) === 'boolean' ? true : false
 }
 
-booWho(null);   // false
-booWho(true);   // true
-booWho(false);   // true
-booWho([1, 2, 3]);   // false
-booWho([].slice);   // false
-booWho({ "a": 1 });   // false
-booWho(1);   // false
-booWho(NaN);   // false
-booWho("a");   // false
-booWho("true");   // false
-booWho("false");   // false
+// booWho(null);   // false
+// booWho(true);   // true
+// booWho(false);   // true
+// booWho([1, 2, 3]);   // false
+// booWho([].slice);   // false
+// booWho({ "a": 1 });   // false
+// booWho(1);   // false
+// booWho(NaN);   // false
+// booWho("a");   // false
+// booWho("true");   // false
+// booWho("false");   // false
