36282286700a7fda8def8f0d33029edd05da1a63
RalucaZota <zotaraluca649@gmail.com>
2025-04-27 19:39:45 +0300

    Falsy-Bouncer.js

--- Algorithm-Scripting/Falsy-Bouncer.js
+++ Algorithm-Scripting/Falsy-Bouncer.js
@@ -5,10 +5,13 @@ Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 */
 
 function bouncer(arr) {
-  return arr.filter(Boolean);
+  const newArr = arr.filter((el) => {
+    return Boolean(el);
+  });
+  return newArr;
 }
 
-bouncer([7, "ate", "", false, 9]);   // [7, "ate", 9]
-bouncer(["a", "b", "c"]);   // ["a", "b", "c"]
-bouncer([false, null, 0, NaN, undefined, ""]);   // []
-bouncer([1, null, NaN, 2, undefined]);   // [1, 2]
+// bouncer([7, "ate", "", false, 9]);   // [7, "ate", 9]
+// bouncer(["a", "b", "c"]);   // ["a", "b", "c"]
+// bouncer([false, null, 0, NaN, undefined, ""]);   // []
+// bouncer([1, null, NaN, 2, undefined]);   // [1, 2]
