8799a1b02675b31cd0e893f015f685de6f1e8e31
RalucaZota <zotaraluca649@gmail.com>
2025-04-29 20:59:01 +0300

    Seek-and-Destroy.js

--- Algorithm-Scripting/Seek-and-Destroy.js
+++ Algorithm-Scripting/Seek-and-Destroy.js
@@ -4,15 +4,12 @@ followed by one or more arguments. Remove all elements from the initial array
 that are of the same value as these arguments.
 */
 
-function destroyer(arr) {
-  var allArguments = arr.slice.call(arguments);
-  var args = allArguments.slice(1);
-  return arr.filter(function(val) {
-    return !args.includes(val);
-  });
+function destroyer(arr, ...args) {
+  const filtered = arr.filter(el => !args.includes(el));
+  return filtered
 }
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);   // [1, 1]
-destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);   // [1, 5, 1]
-destroyer([3, 5, 1, 2, 2], 2, 3, 5);   // [1]
-destroyer([2, 3, 2, 3], 2, 3);   // []
-destroyer(["tree", "hamburger", 53], "tree", 53);   // ["hamburger"]
+const response = destroyer([2, 3, 2, 3], 2, 3);
+// destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);   // [1, 5, 1]
+// destroyer([3, 5, 1, 2, 2], 2, 3, 5);   // [1]
+// destroyer([2, 3, 2, 3], 2, 3);   // []
+// destroyer(["tree", "hamburger", 53], "tree", 53);   // ["hamburger"]
