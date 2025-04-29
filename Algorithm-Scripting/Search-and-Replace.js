a49c2373e5a3362d77b24ca629304892213a7e8b
RalucaZota <zotaraluca649@gmail.com>
2025-04-29 07:58:05 +0300

    Search-and-Replace.js

--- Algorithm-Scripting/Search-and-Replace.js
+++ Algorithm-Scripting/Search-and-Replace.js
@@ -12,17 +12,14 @@ For example if you mean to replace the word "Book" with the word "dog", it shoul
 */
 
 function myReplace(str, before, after) {
-  return str.replace(before, function(){
-    if (before[0] === before[0].toUpperCase()) {
-      return after[0].toUpperCase() + after.slice(1);
-    } else {
-      return after;
-    }
-  });
+  if (before[0] === before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
+  }
+  return str.replace(before, after);
 }
 
-myReplace("Let us go to the store", "store", "mall");   // "Let us go to the mall"
-myReplace("He is Sleeping on the couch", "Sleeping", "sitting");   // "He is Sitting on the couch"
-myReplace("This has a spellngi error", "spellngi", "spelling");   // "This has a spelling error"
-myReplace("His name is Tom", "Tom", "john");   // "His name is John"
-myReplace("Let us get back to more Coding", "Coding", "algorithms");   // "Let us get back to more Algorithms"
+// myReplace("Let us go to the store", "store", "mall");   // "Let us go to the mall"
+// myReplace("He is Sleeping on the couch", "Sleeping", "sitting");   // "He is Sitting on the couch"
+// myReplace("This has a spellngi error", "spellngi", "spelling");   // "This has a spelling error"
+// myReplace("His name is Tom", "Tom", "john");   // "His name is John"
+// myReplace("Let us get back to more Coding", "Coding", "algorithms");   // "Let us get back to more Algorithms"
