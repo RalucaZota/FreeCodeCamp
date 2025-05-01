98184b3c67a2d81d91e4cd3421cc68ebba274a57
RalucaZota <zotaraluca649@gmail.com>
2025-05-01 11:46:44 +0300

    Find-the-Longest-Word-in-a-String.js

--- Algorithm-Scripting/Find-the-Longest-Word-in-a-String.js
+++ Algorithm-Scripting/Find-the-Longest-Word-in-a-String.js
@@ -5,18 +5,43 @@ Your response should be a number.
 */
 
 function findLongestWord(str) {
-  var strToArray = str.split(' ');
-  var longestWord = '';
-  for (i=0; i<strToArray.length; i++) {
-    if (strToArray[i].length > longestWord.length) {
-      longestWord = strToArray[i];
+  const newArr = str.split(' ');
+  let longestLength = 0;
+  let longestWord = '';
+  for (let i = 0; i < newArr.length; i++) {
+    const currentWordLength = newArr[i].length;
+    if (currentWordLength > longestLength) {
+      longestLength = currentWordLength;
+      longestWord = newArr[i];
     }
+    return { length: longestLength, word: longestWord };
   }
-  return longestWord.length;
 }
 
-findLongestWord("The quick brown fox jumped over the lazy dog");   // 6
-findLongestWord("May the force be with you");   // 5
-findLongestWord("Google do a barrel roll");   // 6
-findLongestWord("What is the average airspeed velocity of an unladen swallow");   // 8
-findLongestWord("What if we try a super-long word such as otorhinolaryngology");   // 19
+function findLongestWordSecond(str) {
+  const newArr = str.split(' ');
+  const longestWord = newArr.reduce((acc, currentVal) => {
+    if (currentVal.length > acc) {
+      return currentVal.length;
+    }
+    return acc;
+  }, 0);
+  return longestWord;
+}
+
+function findLongestWordThird(str) {
+  const newArr = str.split(' ');
+  const longestWord = newArr.reduce((acc, currentVal) => {
+    if (currentVal.length > acc.length) {
+      return currentVal;
+    }
+    return acc;
+  }, '');
+  return longestWord;
+}
+
+// findLongestWord("The quick brown fox jumped over the lazy dog");   // 6
+// findLongestWord("May the force be with you");   // 5
+// findLongestWord("Google do a barrel roll");   // 6
+// findLongestWord("What is the average airspeed velocity of an unladen swallow");   // 8
+// findLongestWord("What if we try a super-long word such as otorhinolaryngology");   // 19
