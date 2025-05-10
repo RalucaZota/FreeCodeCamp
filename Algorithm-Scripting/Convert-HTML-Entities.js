caa03b9393f0066804a309d652268990517507f1
RalucaZota <zotaraluca649@gmail.com>
2025-05-10 16:40:31 +0300

    Convert-HTML-Entities.js

--- Algorithm-Scripting/Convert-HTML-Entities.js
+++ Algorithm-Scripting/Convert-HTML-Entities.js
@@ -4,14 +4,20 @@ in a string to their corresponding HTML entities.
 */
 
 function convertHTML(str) {
-    str = str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,"&apos;");
-return str;
+  const htmlEntitiesObj = { '&': '&​amp;', '<': '&​lt;', '>': '&​gt;', '"': '&quot;', "'": '&apos;' };
+  const arr = [...str];
+  const changedArr = arr.map((el) => {
+    if (htmlEntitiesObj[el]) {
+      return htmlEntitiesObj[el];
+    } else return el;
+  });
+  return changedArr.join('');
 }
 
-convertHTML("Dolce & Gabbana");   // Dolce &​amp; Gabbana
-convertHTML("Hamburgers < Pizza < Tacos");   // Hamburgers &​lt; Pizza &​lt; Tacos
-convertHTML("Sixty > twelve");   // Sixty &​gt; twelve
-convertHTML('Stuff in "quotation marks"');   // Stuff in &​quot;quotation marks&​quot;
-convertHTML("Shindler's List");   // Shindler&​apos;s List
-convertHTML("<>");   // &​lt;&​gt;
-convertHTML("abc");   // abc
+convertHTML('Dolce & Gabbana'); // Dolce &​amp; Gabbana
+convertHTML('Hamburgers < Pizza < Tacos'); // Hamburgers &​lt; Pizza &​lt; Tacos
+convertHTML('Sixty > twelve'); // Sixty &​gt; twelve
+convertHTML('Stuff in "quotation marks"'); // Stuff in &​quot;quotation marks&​quot;
+convertHTML("Shindler's List"); // Shindler&​apos;s List
+convertHTML('<>'); // &​lt;&​gt;
+convertHTML('abc'); // abc
