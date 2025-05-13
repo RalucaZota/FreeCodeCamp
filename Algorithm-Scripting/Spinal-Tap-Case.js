1844d67afce5ebb8ee50f17cbf6a64f7a3a0a483
RalucaZota <zotaraluca649@gmail.com>
2025-05-13 08:43:02 +0300

    Spinal-Tap-Case.js

--- Algorithm-Scripting/Spinal-Tap-Case.js
+++ Algorithm-Scripting/Spinal-Tap-Case.js
@@ -3,25 +3,26 @@ Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-da
 */
 
 function spinalCase(str) {
-  str = str.replace(/[A-Z]/g, function(x) {
-    return '-' + x;
-  });
-  var strArr = str.split('');
-  for (i=0; i<strArr.length; i++){
-    if (str[i] === ' ' || strArr[i] === '_'){
-      if (strArr[i+1] === '-'){
-        strArr[i] = '';
-      } else {
-        strArr[i] = '-';
-      }
+  const arr = [...str];
+  const newArr = arr.map((el, index) => {
+    if (el === ' ' || el === '_') return '-';
+    if (
+      el === el.toUpperCase() &&
+      el !== el.toLowerCase() &&
+      index > 0 &&
+      arr[index - 1] !== ' ' &&
+      arr[index - 1] !== '_' &&
+      arr[index - 1] !== '-'
+    ) {
+      return '-' + el.toLowerCase();
     }
-  }
-  if (strArr[0] === '-'){strArr = strArr.slice(1);}
-  return strArr.join('').toLowerCase();
+    return el.toLowerCase();
+  });
+  return newArr.join('');
 }
 
-spinalCase("This Is Spinal Tap");   // "this-is-spinal-tap"
-spinalCase("thisIsSpinalTap");   // "this-is-spinal-tap"
-spinalCase("The_Andy_Griffith_Show");   // "the-andy-griffith-show"
-spinalCase("Teletubbies say Eh-oh");   // "teletubbies-say-eh-oh"
-spinalCase("AllThe-small Things");   // "all-the-small-things"
+spinalCase('This Is Spinal Tap'); // "this-is-spinal-tap"
+spinalCase('thisIsSpinalTap'); // "this-is-spinal-tap"
+spinalCase('The_Andy_Griffith_Show'); // "the-andy-griffith-show"
+spinalCase('Teletubbies say Eh-oh'); // "teletubbies-say-eh-oh"
+spinalCase('AllThe-small Things'); // "all-the-small-things"
