ef66929663c2f3c9a0ae9df5d07c14de507e551f
RalucaZota <zotaraluca649@gmail.com>
2025-05-14 09:05:12 +0300

    Sum-All-Odd-Fibonacci-Numbers.js

--- Algorithm-Scripting/Sum-All-Odd-Fibonacci-Numbers.js
+++ Algorithm-Scripting/Sum-All-Odd-Fibonacci-Numbers.js
@@ -8,23 +8,23 @@ For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less
 */
 
 function sumFibs(num) {
-  var fibList = [1, 1];
-  var sum = 0;
-  var i = 1;
-  while (fibList[i] + fibList[i-1] <= num){
-    fibList.push(fibList[i] + fibList[i-1]);
-    i += 1;
-  }
-  fibList.map(function(number){
-    if (number % 2 !== 0){
-      sum += number;
+  let prev = 0;
+  let curr = 1;
+  let sum = 0;
+  while (curr <= num) {
+    if (curr % 2 !== 0) {
+      sum += curr;
     }
-  });
+    let next = prev + curr;
+    prev = curr;
+
+    curr = next;
+  }
   return sum;
 }
 
-sumFibs(4);   // 5
-sumFibs(1000);   // 1785
-sumFibs(4000000);   // 4613732
-sumFibs(75024);   // 60696
-sumFibs(75025);   // 135721
+sumFibs(4); // 5
+sumFibs(1000); // 1785
+sumFibs(4000000); // 4613732
+sumFibs(75024); // 60696
+sumFibs(75025); // 135721
